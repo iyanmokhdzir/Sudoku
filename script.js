188e391e25b2872e3e27df76ac6cb0b2f067404f
Iyan Mokhdzir <iyan94@gmail.com>
2024-06-24 12:36:30 +0800

    added function to set gridId on tiles, update method to create input element by row and col, fixed setRandomNumOnTiles() so that it validates overlapping num correctly

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let tileIds = [];
+let arrGridIdTileId = [];
 
 window.onload = function () {
   setupGrid();
@@ -9,18 +10,16 @@ window.onload = function () {
 
 function setupGrid() {
   for (let i = 0; i < 9; i++) {
-    const grid = document.createElement("div");
-    grid.id = i.toString();
-    document.getElementById("board-wrapper").appendChild(grid);
-
     for (let j = 0; j < 9; j++) {
       const tile = document.createElement("input");
       tile.id = `${i}${j}`;
+      tile.row = `${i}`;
+      tile.col = `${j}`;
       tile.type = "text";
       tile.maxLength = 1;
       tile.min = 1;
       tile.max = 9;
-      document.getElementById(i).appendChild(tile);
+      document.getElementById("board-wrapper").appendChild(tile);
       tileIds.push(tile.id);
 
       tile.addEventListener("input", function (e) {
@@ -30,33 +29,97 @@ function setupGrid() {
     }
   }
 
-  for (let i = 0; i < 20; i++) {
-    generateRandomNumbersOnTiles();
+  setGridIdOnTiles();
+
+  for (let i = 0; i < 30; i++) {
+    setRandomNumOnTiles();
   }
 }
 
-console.log(tileIds);
+//console.log(tileIds);
 
-function generateRandomNumbersOnTiles() {
-  const randomIndex = Math.round(Math.random() * 81);
-  const randomNum = Math.floor(Math.random() * 9) + 1;
-  const tileId = tileIds[randomIndex];
-  const tile = document.getElementById(tileId);
-
-  if (tile.value == "") {
-    tile.value = randomNum;
-    tile.readOnly = true;
-    tile.style.color = "rgb(132, 55, 204)";
-    
-  } else {
-    generateRandomNumbersOnTiles();
+function setGridIdOnTiles() {
+  let a = 0;
+  for (let h = 0; h <= 54; h += 27) {
+    for (let i = h; i <= h + 6; i += 3) {
+      for (let j = i; j <= i + 18; j += 9) {
+        for (let k = j; k <= j + 2; k++) {
+          let tileId = tileIds[k];
+          const tile = document.getElementById(`${tileId}`);
+          tile.setAttribute("gridid", a);
+          arrGridIdTileId.push([a, tileId]);
+          // console.log(`Index: ${k} - Tile ID: ${tileId} - Grid ID: ${a}`)
+        }
+      }
+      a++;
+    }
   }
 
   /*
-  console.log(randomIndex);
-  console.log(randomNum);
-  console.log(tileId);
+  let test = document.getElementById("04");
+  let test2 = test.getAttribute("gridid"); 
+  console.log(test2); //should return 1
   */
+}
+
+console.log(arrGridIdTileId);
+
+function setRandomNumOnTiles() {
+  const randomTileIndex = generateRandomTileIndex();
+  const tileId = tileIds[randomTileIndex];
+  const tileIdSplit = tileId.split("");
+  const tileRow = tileIdSplit[0];
+  const tileCol = tileIdSplit[1];
+  const tile = document.getElementById(tileId);
+  const gridId = tile.getAttribute("gridid");
+  const randomNum = generateRandomNumber();
+  const gridValues = [],
+    colValues = [],
+    rowValues = [];
+
+  const inputs = document.querySelectorAll(`input[gridid="${gridId}"]`);
+  inputs.forEach((input) => {
+    gridValues.push(input.value);
+  });
+
+  for (let i = 0; i < 9; i++) {
+    const tileCheck = document.getElementById(`${tileRow}${i}`);
+    colValues.push(tileCheck.value);
+  }
+
+  for (let i = 0; i < 9; i++) {
+    const tileCheck = document.getElementById(`${i}${tileCol}`);
+    rowValues.push(tileCheck.value);
+  }
+
+  console.log(`tile Id: ${tileId}`);
+  console.log(`random num: ${randomNum}`);
+  console.log(`grid Id: ${gridId}`);
+  console.log(`row: ${tileRow}`);
+  console.log(`col: ${tileCol}`);
+  console.log(gridValues);
+  console.log(rowValues);
+  console.log(colValues);
+  console.log(`is tile empty ?: ${(tile.value == "")}`);
+  console.log(`random num included in grid ?: ${gridValues.includes(randomNum.toString())}`);
+  console.log(`random num included in col ?: ${colValues.includes(randomNum.toString())}`);
+  console.log(`random num included in row ?: ${rowValues.includes(randomNum.toString())}`);
+
+  if (tile.value == "" && !(gridValues.includes(randomNum.toString())) && !(colValues.includes(randomNum.toString())) && !(rowValues.includes(randomNum.toString()))) {
+    tile.value = randomNum;
+    tile.readOnly = true;
+    tile.style.color = "rgb(207, 102, 23)";
+  } else {
+    setRandomNumOnTiles();
+  }
+}
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 9) + 1;
+}
+
+function generateRandomTileIndex() {
+  return Math.floor(Math.random() * tileIds.length);
 }
 
 function validateEmptyTiles() {
@@ -78,29 +141,4 @@ function validateEmptyTiles() {
 
 function validateAnswer() {
   console.log("tiles are not empty");
-
-  let arrCol = [];
-  let arrRow = [];
-
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      const tileRow = document.getElementById(`${i}${j}`);
-      const tileCol = document.getElementById(`${j}${i}`);
-
-      if (!arrRow.includes(tileRow.value)) {
-        arrRow.push(tileRow.value);
-      } else {
-        tileRow.style.backgroundColor = "rgb(232, 176, 155)";
-      }
-
-      if (!arrCol.includes(tileCol.value)) {
-        arrCol.push(tileCol.value);
-      } else {
-        tileCol.style.backgroundColor = "rgb(232, 176, 155)";
-      }
-    }
-
-    arrCol = [];
-    arrRow = [];
-  }
 }
